dbd67c41b6415f308b235e9d70d6bd44b7d15da3
Nicolas Bertollo <nico26200@hotmail.com>
2020-02-12 14:15:26 +0100

    v1

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -4,14 +4,31 @@
  * https://HosseinKarami.github.io/fastshell
  * @author Hossein Karami
  * @version 1.0.5
- * Copyright 2016. MIT licensed.
+ * Copyright 2020. MIT licensed.
  */
 (function ($, window, document, undefined) {
 
   'use strict';
 
   $(function () {
-    // FastShell
+    var ctx = document.getElementById('myChart');
+    var myLineChart = new Chart(ctx, {
+      type: 'line',
+      xAxisID: 10000,
+      data: {
+        labels: ['Monday','Thuesday','wendesday','thuesday','friday','saturday','sunday'],
+        datasets: [{ 
+            data: [1200,1180,1000,750,750,600,250],
+            borderColor: "#3e95cd",
+            fill: false,
+            label: 'Week depense'
+          }
+        ]
+      },
+      options: {
+        
+      }
+    });
   });
 
 })(jQuery, window, document);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,24 @@
   'use strict';
 
   $(function () {
-    // FastShell
+    var ctx = document.getElementById('myChart');
+    var myLineChart = new Chart(ctx, {
+      type: 'line',
+      xAxisID: 10000,
+      data: {
+        labels: ['Monday','Thuesday','wendesday','thuesday','friday','saturday','sunday'],
+        datasets: [{ 
+            data: [1200,1180,1000,750,750,600,250],
+            borderColor: "#3e95cd",
+            fill: false,
+            label: 'Week depense'
+          }
+        ]
+      },
+      options: {
+        
+      }
+    });
   });
 
 })(jQuery, window, document);
